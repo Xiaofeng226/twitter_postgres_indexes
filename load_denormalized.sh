280d8413854f9a15bfa99e8b12bc4f3dc9653c04
Xiaofeng226 <xiaofengli878@gmail.com>
2026-05-05 18:27:55 -0700

    idk what is happening

---
+++ load_denormalized.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+file=$1
+unzip -p $file | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:1543/postgres -c "\COPY tweets_jsonb (data) FROM STDIN CSV QUOTE e'\x01' DELIMITER e'\x02'"
--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 files='/data/tweets/geoTwitter21-01-01.zip
 /data/tweets/geoTwitter21-01-02.zip
@@ -11,19 +11,12 @@ files='/data/tweets/geoTwitter21-01-01.zip
 /data/tweets/geoTwitter21-01-09.zip
 /data/tweets/geoTwitter21-01-10.zip'
 
-
 echo '================================================================================'
 echo 'load pg_denormalized'
 echo '================================================================================'
-# FIXME: copy your solution to the previous problem here
-
-# NOTE:
-# I have removed the pg_normalized code from this repo.
-# The only difference between pg_normalized and pg_normalized_batch is how the data is loaded.
-# Since pg_normalized_batch is faster,
-# we will use that code to load the data.
+time echo "$files" | parallel bash load_denormalized.sh
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-# FIXME: copy your solution to the previous problem here
+time echo "$files" | parallel python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:5321/postgres --inputs
