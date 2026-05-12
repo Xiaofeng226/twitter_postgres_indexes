df8665dce5fc61fa913533d18ad4d018d638594b
Xiaofeng226 <xiaofengli878@gmail.com>
2026-05-12 15:16:05 -0700

    finished

---
+++ sql.denormalized/02.sql
@@ -0,0 +1,15 @@
+SELECT tag, count(*) as count
+FROM (
+    SELECT DISTINCT data->>'id' as id_tweets,
+        '#' || (jsonb->>'text') as tag
+    FROM tweets_jsonb,
+        jsonb_array_elements(
+            COALESCE(data->'entities'->'hashtags','[]') ||
+            COALESCE(data->'extended_tweet'->'entities'->'hashtags','[]')
+        ) as jsonb
+    WHERE data->'entities'->'hashtags' @> '[{"text": "coronavirus"}]'
+       OR data->'extended_tweet'->'entities'->'hashtags' @> '[{"text": "coronavirus"}]'
+) t
+GROUP BY tag
+ORDER BY count DESC, tag
+LIMIT 1000;
---
+++ sql.denormalized/04.sql
@@ -0,0 +1,4 @@
+SELECT count(distinct data->>'id')
+FROM tweets_jsonb
+WHERE to_tsvector('english', COALESCE(data->'extended_tweet'->>'full_text', data->>'text')) @@ to_tsquery('english', 'coronavirus')
+  AND data->>'lang' = 'en';
