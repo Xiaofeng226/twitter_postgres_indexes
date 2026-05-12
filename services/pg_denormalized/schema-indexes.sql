c8ebf6d91e8f76df6064721ed3b1d279bc3334fe
Xiaofeng226 <xiaofengli878@gmail.com>
2026-05-12 15:17:01 -0700

    forgot to add the denormalized schema

---
+++ services/pg_denormalized/schema-indexes.sql
@@ -0,0 +1,6 @@
+SET max_parallel_maintenance_workers=4;
+SET maintenance_work_mem='1GB';
+CREATE INDEX idx_tweets_jsonb_lang ON tweets_jsonb ((data->>'lang'));
+CREATE INDEX ON tweets_jsonb USING GIN((data->'entities'->'hashtags') jsonb_path_ops);
+CREATE INDEX ON tweets_jsonb USING GIN((data->'extended_tweet'->'entities'->'hashtags') jsonb_path_ops);
+CREATE INDEX ON tweets_jsonb USING GIN(to_tsvector('english', COALESCE(data->'extended_tweet'->>'full_text', data->>'text')));
